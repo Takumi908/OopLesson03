549c41bda1d84712c775f55ac35adaccf264c422
TakumiYoshida <31140@ojs.ac.jp>
2020-10-01 10:23:55 +0900

    10/1

--- Chapter04/Program.cs
+++ Chapter04/Program.cs
@@ -7,6 +7,18 @@ using System.Threading.Tasks;
 namespace Chapter04 {
     class Program {
         static void Main(string[] args) {
+            string code = "12345";
+
+            var message = GetMessage(code) ?? DefaultMessage();
+            Console.WriteLine(message);
+        }
+         //スタブ
+        private static object DefaultMessage() {
+            return "DefaultMessage";
+        }
+        //スタブ
+        private static object GetMessage(string code) {
+            return 10;
         }
     }
 }
